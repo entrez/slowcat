2c274d08131d21180cd548d196dcd55ade9bfd4d
Michael Meyer <me@entrez.cc>
2022-05-26 19:49:07 -0400

    Support -d <msec> in addition to -d<msec>

--- slowcat.c
+++ slowcat.c
@@ -41,23 +41,39 @@ int
 main(int argc, char *argv[])
 {
     int argnum = 1;
+    const char *arg;
     if (argc < 2) {
         int i;
         for (i = 0; usage[i]; i++) {
-            printf("%s\n", usage[i]);
+            fprintf(stderr, "%s\n", usage[i]);
         }
         return 0;
     }
 
-    if (!strncmp("-d", argv[1], 2)) {
-        char *delay_ptr = argv[1] + 2;
-        delay_time = atoi(delay_ptr);
-        delay_time *= 1000;
+    arg = argv[1];
+    if (!strcmp("--", arg)) {
+        /* if a filename starts with -d, invoke slowcat like
+         *  $ slowcat -- -deep.txt
+         * and it will work; otherwise it will think it's the -d option */
         argnum++;
+    } else if (!strncmp("-d", arg, 2)) {
+        const char *delay_ptr = arg + 2;
+        int dtmp;
+        if ((dtmp = atoi(delay_ptr)) == 0) {
+            if (argc > 2)
+                dtmp = atoi(argv[2]);
+            argnum++;
+        }
+        if (dtmp) {
+            delay_time = (useconds_t) dtmp * 1000;
+            argnum++;
+        } else {
+            fprintf(stderr, "slowcat: ignoring -d: no time specified\n");
+        }
     }
 
     for (; argnum < argc; argnum++) {
-        const char *arg = argv[argnum];
+        arg = argv[argnum];
         if (slowcat(arg)) {
             char name[ERRLEN]; 
             snprintf(name, ERRLEN, "slowcat: %s", arg);
